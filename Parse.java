bfd26530bacd6fc3a369d2316ffaf2ad3bb8a32d
PandaNinja10 <78180593+PandaNinja10@users.noreply.github.com>
2023-05-27 15:37:03 -0700

    Create Parse.java

---
+++ Parse.java
@@ -0,0 +1,11 @@
+public class Parse {
+  
+  // stuff for number conversion
+  // 
+  // char to digit
+
+  // string to double
+    // get inegerpart until decimal or end
+    // if decimal stop int start decimal till end
+  }
+}
